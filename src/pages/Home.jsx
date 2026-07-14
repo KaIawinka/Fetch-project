f3914dddc4f557145b401f1e5b0c57c293ff177d
KaIawinka <standofferm555@gmail.com>
2026-07-14 17:16:10 +0600

    New style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import styles from './Home.module.css'
 
 function Home() {
 	return (
-		<>
-				Home
-		</>
+		<div className={styles.wrapper}>
+			<div className={styles.glow} />
+
+			<main className={styles.card}>
+				<span className={styles.badge}>Добро пожаловать</span>
+				<h1 className={styles.title}>Home</h1>
+				<p className={styles.subtitle}>
+					Начните исследовать
+				</p>
+
+				<div className={styles.actions}>
+					<button className={styles.primaryBtn}>Начать</button>
+					<button className={styles.secondaryBtn}>Подробнее</button>
+				</div>
+			</main>
+		</div>
 	)
 }
 
